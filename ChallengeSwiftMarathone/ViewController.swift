48eeba3c311581117bd0d1ea8d50c3ad25a57e4a
Danila Bolshakov <danila1999777@gmail.com>
2023-08-21 09:59:18 +0300

    Initial commit

---
+++ ChallengeSwiftMarathone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeSwiftMarathone
+//
+//  Created by iOS - Developer on 21.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
